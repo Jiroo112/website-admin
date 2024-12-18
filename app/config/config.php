936b1924bf19ed6bbe1b0b3416992f82c8452991
jessichaalvina <146162362+jessichaalvina@users.noreply.github.com>
2024-12-19 03:36:41 +0700

    melakukan revisi edit profile

--- app/config/config.php
+++ app/config/config.php
@@ -5,4 +5,4 @@ define('BASEURL', 'http://localhost/admin-adek/public/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'db_adek');
+define('DB_NAME', 'adek');
